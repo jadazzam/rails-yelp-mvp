628b4b9e2c84c17a9ea58b921ee2014246212349
Jad Azzam <jadazzam@me.com>
2019-04-26 15:02:36 +0200

    ok

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,34 @@
 class ReviewsController < ApplicationController
   def new
-      # we need @restaurant in our `simple_form_for`
-      @restaurant = Restaurant.find(params[:restaurant_id])
-      @review = Review.new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
     end
 
-    def create
-      @review = Review.new(review_params)
-      @review.restaurant = Restaurant.find(params[:restaurant_id])
-      if @review.save
-        redirect_to restaurant_path(@restaurant)
-      else
-        render :new
-      end
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
     end
+  end
 
-    private
+  private
 
-    def review_params
-      params.require(:review).permit(:content, :rating)
-    end
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
+
+# def create
+#   @review = Review.create(review_params)
+#   @review.restaurant = Restaurant.find(params[:restaurant_id])
+#   if @review.save
+#     redirect_to restaurant_path(@restaurant)
+#   else
+#     render :new
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants do
-    resources :reviews, only: [ :show, :edit, :update, :destroy ]
+    resources :reviews
   end
   # get 'restaurants/:restaurant_id/reviews', to: 'reviews#index'
 
